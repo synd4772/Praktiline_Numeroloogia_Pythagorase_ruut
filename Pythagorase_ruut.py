4ec01506c631c99b49272b82264de137b470b94b
opilane <opilane@HP-CZC2349HT9>
2024-03-15 11:40:09 +0200

    Add project files.

---
+++ Pythagorase_ruut.py
@@ -0,0 +1,65 @@
+﻿data = {
+    1: {
+        0:"",
+        1:"Kui on ainult üks ühik, siis see inimene on rafineeritud egoist. Pööramata tähelepanu millelegi, püüab ta igast positsioonist saada maksimaalset kasu eelkõige iseendale. Teda ei huvita eriti teised inimesed ega see, mida ja kuidas nad elavad. Peaasi, et ta elab ja peab elama ainult üksi.",
+        11:"Inimesel on kergelt väljendunud isekus (kogu aeg kiidab ennast nagu müüki, kardab üle hinnata)",
+        111:"Inimene on hea iseloomuga, postikuluga. Selliseid inimesi on küllalt raske endast välja viia ja nendega on alati võimalik ühist keelt leida.",
+        1111:"Tugeva iseloomuga, tahtejõuline mees. Ta ei talu valetamist, kuigi mõnikord võib ta hammastega kokku klammerdudes teha seda ka lähedaste inimeste pärast. Sellise iseloomuga mehed sobivad professionaalsete sõjaväelaste rolli, naised hoiavad tavaliselt oma peret rusikas.",
+        11111:"Inimene on diktaator, samost. Oma idee või kapriisi nimel võib tuulde lasta miljoneid - kas inimelusid või raha. Oma eesmärgi saavutamiseks läheb see, mida nimetatakse laipadeks, ilma et peaks tagasi vaatama ja seisma jääma. Kui tal pole võimalust oma tähtsust tunda, võib ta saada türanniks perekonnas, erakuks või omandada psüühikahäireid. Vastupidav kavatsustes, kättemaksuks, kohaneb hästi. Tihti jälitame mingit tõsist maaniat. Ta on oma laste või lastelastega seotud. Konkreetne - püüab kõike ette mõelda.",
+        111111:"See kombinatsioon on haruldane. Inimene on tavaliselt karm, vägivaldne, kuid lähedastele või 'inimkonna hüvanguks' on ta võimeline sooritama võimatuid asju. Inimesed on väga ebaharilikud, tihtipeale ühe idee mõju all. Nendega on raske suhelda, raske võistelda.",
+    },
+    2: {
+        0:"",
+        2:"Bioenergeetiliselt tavalised inimesed. Kuid bioenergiat on vähe. Need inimesed peaksid hoiduma pingelistest olukordadest, liiga tugevatest emotsioonidest, kehalise kasvatuse või spordiga tegelemisest, kõige tõhusam on jooga. Sellised inimesed on väga tundlikud atmosfäärimuutuste suhtes.",
+        22:"On piisavalt bioenergiat, suudab jagada energiat teistega, võib saada tervendajaks. Selliste inimeste peres on põhimõtteliselt harva närvipingeid, kui just suhteid ei raskenda mingid erilised asjaolud.",
+        222:"Selgeltnägija märk. On võimalik, et inimene ise ei tea oma võimetest midagi, kuid sellised inimesed on kõige kohanemisvõimelisemad oma bioväljaga ravima. Oma võimete arendamiseks piisab vaid väikesest tõukest.",
+        2222:"Kui te olete kunagi kohanud naist, kellesse kõik on armunud ilma meheta või meest, kes on kõigile naistele vastupandamatu, siis te kindlasti veendute, et neil on need kaks. Neile inimestele meeldib väga vastassugupool. Kuid need inimesed, kellel on kõrge bioenergia, peaksid olema ettevaatlikud: need, kellel on 'saatana märk' - nad ammutavad neis energiat, toidavad seda, pumpavad seda sõna otseses mõttes."
+    },
+    3: {
+        0:"Tavaliselt on nad väga puhtad või täpsed inimesed; midagi eraldab neid ümbritsevast keskkonnast - nad puhastavad pidevalt midagi, rügavad; naised, kellel ei ole kolmikuid, on suurepärased perenaised, kuid mõnevõrra ärritavad oma puhtuse taotlemisega; mehed jälgivad hoolikalt oma välimust ja ei talu, kui nende sõbrannad jäävad kohtingutele hiljaks. Lapsi kasvatatakse rangelt. Nad armastavad end peeglist vaadata, öelda tarku, vitioosseid fraase. Hooletut tööd tehakse laitmatult.",
+        3:"Neid inimesi ei häiri kord, kõik sõltub tujust, 'tahan teha, tahan ei'.",
+        33:"Selgelt väljendub kalduvus teadusele. Analüütiline mõtteviis. Head matemaatikud, füüsikud, keemikud, teadlased.",
+        333:"Tugevnenud kalduvus teadusele; kui see ei realiseeru, tekitab see võimatut pedantsust, igavust, kalduvust pidevale 'õigluse taastamisele'.",
+        3333:"Suur viis, ei ole soov areneda"
+    },
+    4: {
+        0:"- Tervis on väga nõrk, inimene on lapsepõlvest saati vastuvõtlik erinevatele haigustele; valu on seda suurem, mida rohkem on tal kahte, sest nii või teisiti on ta sunnitud andma oma energiat",
+        4:"Tervis on keskmine, keha on vaja karastada; haigused lähenevad vanadusele.",
+        44:"- Loodusest on ta väga tugev ja kõrge palavikuga mees.",
+        444:"Inimesed, kellel on väga tugev tervis ja suured seksuaalsed võimalused. Looduse mehed on füüsiliselt väga tugevad."
+    },
+    5: {
+        0:"Avatud kanal sündides, nii et see inimene püüab alati midagi ette võtta, midagi tõestada, alati on pea segaduses, ise eksperimendis, arvestuses. Elukogemus näitab, et selle inimesega on raske koos elada. Kõik saab (taob) oma peaga.",
+        5:"Sidekanal on avatud. Need inimesed teevad palju vähem vigu ka elus ja üldse, suudavad olukorda õigesti arvutada ja sellest maksimaalset kasu saada.",
+        55:"Tugevalt arenenud intuitsioon. Nad võivad asjade käiku ette aimata. Nende jaoks kõige sobivamad ametid on jurist, uurija.",
+        555:"Peaaegu selgeltnägijad. Sellised inimesed teavad alati, kuidas ühes või teises olukorras toimida, nii et nad peaaegu ei tee vigu. Paljudel juhtudel ennustatakse täpselt eelseisvaid sündmusi.",
+        5555:"Selgeltnägijad. Kõik, mis ümberringi toimub, on neile täiesti selge. Nende elus on hetki, mil nad on väljaspool aega ja ruumi. Isiksused, kes oma võimeid täielikult realiseerivad, väljuvad tavalise inimese raamidest."
+    },
+    6: {
+        0:"Selline inimene on tulnud maa peale käsitööd omandama, füüsiline töö on hädavajalik, kuid ta ei armasta seda.",
+        6:"Inimene on mingil määral maandatud, võib tegeleda loovuse või täppisteadustega, kuid füüsiline töö on olemasolu eeltingimus.",
+        66:"Inimene on väga maandatud. Need inimesed pingutavad füüsilise töö nimel, kuigi see pole neile kohustuslik.",
+        666:"- Saatana märk. Kõrgema temperamendiga inimesed, sarmikad, muutudes pidevalt ühiskonnas tähelepanu keskpunktiks. Partner, kellega ta on abielus, peab olema palju kahekesi. ta toitub oma partneri ja sageli pumbates energiat läheb teise partneri.",
+        6666:"See mees on oma varasemates kehastustes saanud palju maandust, ta teeb väga palju tööd. Tema jaoks ei ole raskusi (füüsilist), ta teeb alati tööd. Kindlasti on vaja instituuti, kui seal on 9-k."
+    },
+    7: {
+        0:"Этот человек родился, чтобы зарабатывать семерки, а заработать их можно только стараниями. Очень тяжелая жизнь. Почти всегда приведет к религии.",
+        7:"Jumala säde. Sellised inimesed elavad palju kergemini, neil on annet, kuid nad ei ole selgelt väljendatud.",
+        77:"- See on väga tugev märk ingliabist, kui seda täielikult arendada. Inimene on väga andekas, muusikaline, kunstilise maitsega, suudab joonistada. Kui on kaks ühikut, võib tema isekus juhtida teda ja tema annet. Selline inimene kõnnib raseerimisteradel, tal õnnestub kõik, hea ja halb. Tema jaoks pole suletud uksi. Kui ta läheb kohtusse, siis on tal kindlasti abi protsessi võitmiseks või august väljatoomiseks. Lapsepõlvest tuleb õppida altruismi.",
+        777:"Märk on eriline. Need inimesed tulevad maa peale tavaliselt lühikeseks ajaks, ja kui nad tulevad oma eluraskustega, ootab neid vanaduspõlv halvatus.",
+        7777:"Ingli märk. Sellise märgiga inimesed surevad lapsekingades ja kui nad jäävad elama, ähvardab neid pidevalt oht või katastroof."
+    },
+    8: {
+        0:"See vastab peaaegu puudulikule kohusetundele. Inimene ei pea end kunagi kellelegi kohustuseks, ta ei kiirusta seda andma ja jätab kõik määratud tähtajad vahele.",
+        8:"Võib öelda, et neile inimestele on antud see, mida nimetatakse kohusetundeks: nad on vastutustundlikud, kohusetundlikud, täpsed.",
+        88:"Väga arenenud kohusetunne. Neid inimesi eristab alati soov aidata teisi, eriti nõrku, haigeid, üksikuid. Nad arvavad, et nad vastutavad kellegi või millegi eest. Tavaliselt on toredad perenaised.",
+        888:"See on suur märk. Märk rahva teenimisest. Sellised inimesed kas juhivad rahvaid, et juhtida neid valitud teel, või kui nad jäävad varju, ei kahetse ennast ega säästa jõudu, töötavad nad nende hüvanguks. Igal juhul saavutab valitseja kolme kaheksaga silmapaistvaid tulemusi.",
+        8888:"Märk tuleb alles 88. aastal. Selle märgiga lapsed on sündinud parapsühholoogiliste võimetega ja erakordse vastuvõtlikkusega täppisteadustele. Neile on avatud üleloomulikud teed."
+    },
+    9: {
+        0:"Inimene ei hiilga sünnist saati mõistusega, ta peab vaimselt arenema, palju ja kõvasti õppima, kuid see ei ole talle lihtne.",
+        9:"Inimene peab tegema kõvasti tööd, et teenida vähemalt üks üheksa.",
+        99:" Mehel on sünnist saati tark pea. Nad peavad õppima, sest. See võib palju korda saata.",
+        999:"Väga targad loodusest, nõrgad õppijad. Kõik on antud. Õpetuses ei pingutata üldse: kõik, mis nõuab neilt mingeid vaimseid kulutusi, ei valmista neile raskusi.",
+        9999:"Neile inimestele avaneb tõde. Kui intuitsioon on arenenud, on see garanteeritud, et see ei õnnestu ühelgi oma ettevõtmisel. Kõige selle juures on nad tavaliselt üsna ebameeldivad, sest terav mõistus muudab nad ebaviisakaks, halastamatuks ja vägivaldseks. Nendest ei ole mingit kahju. Pole mingit mõtet raisata vaeva, et nad oma meelt muudaksid. Sageli on nad väga põlastusväärsed."
+    }}
